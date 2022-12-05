38bdd9928f83d30b929bf30c643d85d3b1567720
Oleg <hillott@yandex.ru>
2022-12-05 08:57:54 +0300

    упражнение 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean clientOs = false;
+        if (clientOs) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для iOS по ссылке") ;
+        }
+    }
+}
