54285a087997c6917fd11ee62ea071d8c187f013
Oleg <hillott@yandex.ru>
2022-12-05 09:15:25 +0300

    упражнение 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        boolean clientOs = false;
+        boolean clientOs = true;
         if (clientOs) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         else {
             System.out.println("Установите версию приложения для iOS по ссылке") ;
+        }
+
+        //задание 2
+        int clientDeviceYear = 2014;
+
+
+        if (clientDeviceYear >= 2015) {
+            System.out.println("установите наше приложение по ссылке");
+        }
+        else if (clientOs == true) {
+            System.out.println("установите облегченную версию для Android");
+        }
+        else {
+            System.out.println("установите облегченную версию для iOs");
         }
     }
 }
